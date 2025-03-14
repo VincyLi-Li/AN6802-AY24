ef5c6de67d726a2a04bce84e6b35e8623728e04a
VincyLi-Li <li_wenxin023@163.com>
2025-03-14 11:53:10 +0000

    1

--- app.py
+++ app.py
@@ -11,6 +11,15 @@ def main():
     user_name=request.form.get("q")
     return(render_template("main.html"))
 
+@app.route("/foodexp", methods=["POST", "GET"])
+def foodexp():
+    return(render_template("foodexp.html"))
+
+@app.route("/foodexp_pred", methods=["POST", "GET"])
+def foodexp_pred():
+    q=float(request.form.get("q"))
+    return(render_template("foodexp_pred.html",r=(q*0.48517842)+147.4))
+
 @app.route("/ethical_test", methods=["POST", "GET"])
 def ethical_test():
     return(render_template("ethical_test.html"))
@@ -18,7 +27,7 @@ def ethical_test():
 @app.route("/test_result", methods=["POST", "GET"])
 def test_result():
     answer=request.form.get("answer")
-    if answer=="false":
+    if answer=="false": 
         return(render_template("pass.html"))
     elif answer=="true":
         return(render_template("fail.html"))

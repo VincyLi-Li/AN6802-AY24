f68d9357998bc756897ae189d6fee2102f551343
VincyLi-Li <li_wenxin023@163.com>
2025-03-28 12:52:45 +0000

    1

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import sqlite3
 import datetime
 import google.generativeai as genai
 import os
+import wikipedia
 
-api="AIzaSyD9DSPOTsZb1AL90de5IWs2G0Yi6yJEEJI"
+api=os.getenv("makersuite")
 model=genai.GenerativeModel("gemini-1.5-flash")
 genai.configure(api_key=api)
 
@@ -35,6 +36,14 @@ def main():
 def foodexp():
     return(render_template("foodexp.html"))
 
+@app.route("/foodexp1", methods=["POST", "GET"])
+def foodexp1():
+    return(render_template("foodexp1.html"))
+
+@app.route("/foodexp2", methods=["POST", "GET"])
+def foodexp2():
+    return(render_template("foodexp2.html"))
+
 @app.route("/foodexp_pred", methods=["POST", "GET"])
 def foodexp_pred():
     q=float(request.form.get("q"))
@@ -60,6 +69,12 @@ def FAQ():
 def FAQ1():
     r=model.generate_content("Factors for Profit")
     return(render_template("FAQ1.html",r=r.candidates[0].content.parts[0]))
+
+@app.route("/FAQinput", methods=["POST", "GET"])
+def FAQinput():
+    q=request.form.get("q")
+    r=wikipedia.summary(q)
+    return(render_template("FAQinput.html",r=r))
 
 @app.route("/userlog", methods=["POST", "GET"])
 def userlog():
